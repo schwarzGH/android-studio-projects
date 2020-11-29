8d67293b36a41aaaa0c1a3fccda50e87fad8f24f
schwarz <63720576+schwarzGH@users.noreply.github.com>
2020-11-29 19:17:58 +0700

    parsing json to sandwich object with gson

--- sandwich_club/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ sandwich_club/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,21 @@
 package com.udacity.sandwichclub.utils;
 
+import com.google.gson.Gson;
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
+    protected static Gson gson = new Gson();
+
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        return gson.fromJson(json, Sandwich.class);
     }
+
 }
